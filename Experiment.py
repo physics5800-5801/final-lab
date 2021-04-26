f4798857e480947820c336eca18a104ee03c1507
Conner Graham <59581008+cgraham1092@users.noreply.github.com>
2021-04-26 10:36:00 -0400

    Started basic class implementation

--- Experiment.py
+++ Experiment.py
@@ -14,6 +14,13 @@ Shannon entropy and frequency normalization.
 ## Imports 
 ######################################################################
 
+import re
+import unicodedata
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from termcolor import cprint
 import LightSource as source
 
 ######################################################################
@@ -34,12 +41,197 @@ __status__ = "Development"
 # Class Definition
 ######################################################################
 
-class Experiment:
+class Experiment(object):
     """Class docstrings go here TODO."""
-    def __init__(self, name):
+    __PLANKS_CONSTANT = 6.62607015e-34
+
+    def __init__(self, name="unnamed"):
         """Class method docstrings go here TODO."""
-        self.name = name
+        slug_name = self.__slugify(name)
+        self.__name = 'unnamed' if (slug_name == '') else slug_name
+        self.__datalog = []
+        self.__energy_df = None
+        self.__plank = 0
+        self.__worf_func = 0
+        return
+
+    def __slugify(self, value, allow_unicode=False):
+        """Taken from https://github.com/django/django/blob/master/django/utils/text.py.
+        Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+        dashes to single dashes. Remove characters that aren't alphanumerics,
+        underscores, or hyphens. Convert to lowercase. Also strip leading and
+        trailing whitespace, dashes, and underscores.
+
+        Parameters
+        ----------
+        value : string
+            TODO.
+        allow_unicode : bool, optional
+            TODO.
+
+        Returns
+        -------
+        slug_value : string
+            TODO.
+        """
+
+        value = str(value)
+        if allow_unicode:
+            value = unicodedata.normalize('NFKC', value)
+        else:
+            value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+        value = re.sub(r'[^\w\s-]', '', value.lower())
+        return re.sub(r'[-\s]+', '-', value).strip('-_')
 
     def get_name(self):
         """Class method docstrings go here TODO."""
-        return self.name
+        return self.__name
+
+    def get_option_range(self):
+        option_keys = self.__options.keys()
+        return (min(option_keys), max(option_keys))
+
+    def display_options(self):
+        """
+        TODO.
+        """
+
+        print('\n------------------------- EXPERIMENT OPTIONS -------------------------')
+        for i in range(len(self.__options)):
+            print('{indx: >4}. {desc}'.format(indx=i, desc=self.__options[i][1]))
+        print('----------------------------------------------------------------------')
+        return
+
+    def process_option(self, option):
+        print('OPTION ' + str(option) + ': ' +self.__options[option][1])
+        return self.__options[option][0](self)
+    
+    def __quit(self):
+        print('Confirmation - Unsaved data will be lost.')
+        confirmation = input('Would you like to proceed (y/n)?: ').lower()
+        while (confirmation != 'y' and confirmation != 'n'):
+            cprint("ERROR: invalid input: please enter either 'y' or 'n'", 'red')
+            confirmation = input('Would you like to proceed (y/n)?: ').lower()
+        return (True if (confirmation == 'y') else False)
+
+    def __add_log_entry(self):
+        source_type = input('Is this an LED or a Laser?: ').upper()
+        while (source_type != 'LED' and source_type != 'LASER'):
+            cprint("ERROR: invalid input: please enter either 'LED' or 'Laser'", 'red')
+            source_type = input('Is this an LED or a Laser?: ').upper()
+        if (source_type != 'LED'): source_type = source_type.capitalize()
+        wavelength_nm = 0
+        while (wavelength_nm <= 0):
+            try:
+                wavelength_nm = float(input('Enter the {} wavelength in nm: '.format(source_type)))
+                if (wavelength_nm <= 0): cprint('ERROR: invalid input: wavelength must be a positive real number', 'red')
+            except:
+                cprint('ERROR: invalid input: wavelength must be a positive real number', 'red')
+        
+        entry = source.Light_Source(wavelength_nm, source_type)
+        from_file = input('Load data from csv file (y/n)?: ').lower()
+        while (from_file != 'y' and from_file != 'n'):
+            cprint("ERROR: invalid input: please enter either 'y' or 'n'", 'red')
+            from_file = input('Load data from csv file (y/n)?: ').lower()
+        if (from_file == 'y'):
+            try:
+                entry.load_data_from_csv(input('Enter csv file path: '))
+            except:
+                cprint('ERROR: read_csv: unable to load file', 'red')
+                return
+        else:
+            entry.collect_data()
+        print('[+]Added entry:')
+        print('->', entry)
+        entry.plot_data()
+        self.__datalog += [entry]
+        return
+
+    def __remove_log_entry(self):
+        if (len(self.__datalog) > 0):
+            self.__display_log()
+            try:
+                indx = int(input('Select the entry to remove: '))
+                if (indx >= 0 and indx < len(self.__datalog)):
+                    entry = self.__datalog.pop(indx)
+                    print('[+]Removed entry:')
+                    print('->', entry)
+                else:
+                    cprint('ERROR: invalid entry: no entries were removed', 'red')
+            except:
+                cprint('ERROR: invalid entry: no entries were removed', 'red')
+        else:
+            print('[+]The datalog is already empty')
+        return
+
+    def __update_log_entry(self):
+        if (len(self.__datalog) > 0):
+            x = 0
+        else:
+            print('[+]The datalog is currently empty')
+        return
+
+    def __display_log(self):
+        log_size = len(self.__datalog)
+        if (log_size > 0):
+            print('--------------- Datalog for {name} ---------------'.format(name=self.__name))
+            for i in range(log_size):
+                print('{indx: >2}. '.format(indx=i), self.__datalog[i])
+        else:
+            print('[+]The datalog is currently empty')
+        return
+
+    def __view_log_entry(self):
+        log_size = len(self.__datalog)
+        if (log_size > 0):
+            self.__display_log()
+            try:
+                indx = int(input('Select the entry to view: '))
+                if (indx >= 0 and indx < len(self.__datalog)):
+                    entry = self.__datalog[indx]
+                    print('{i: >2}. '.format(i=indx), entry, '\n')
+                    print(entry.get_data())
+                    entry.plot_data()
+                else:
+                    cprint('ERROR: invalid entry: unable to view entry', 'red')
+            except:
+                cprint('ERROR: invalid entry: unable to view entry', 'red')
+        else:
+            print('[+]The datalog is currently empty')
+        return
+
+    def __save_log(self):
+        if (len(self.__datalog) > 0):
+            out_dir = './' + self.__name
+            if (not os.path.exists(out_dir)):
+                os.mkdir(out_dir)
+            for i in range(len(self.__datalog)):
+                entry = self.__datalog[i]
+                path = out_dir + '/' + entry.get_type().lower() + '_' + str(round(entry.get_wavelength())) + 'nm.csv'
+                entry.get_data().to_csv(path, encoding='utf-8', index=False)
+        else:
+            print('[+]The datalog is currently empty')
+        return
+
+    def __clear_log(self):
+        if (len(self.__datalog) > 0):
+            print('Confirmation - This action cannot be undone.')
+            confirmation = input('Would you like to proceed (y/n)?: ').lower()
+            while (confirmation != 'y' and confirmation != 'n'):
+                cprint("ERROR: invalid input: please enter either 'y' or 'n'", 'red')
+                confirmation = input('Would you like to proceed (y/n)?: ').lower()
+            if (confirmation == 'y'):
+                self.__datalog.clear()
+                print('[+]The datalog has been cleared')
+        else:
+            print('[+]The datalog is already empty')
+        return
+
+    __options = {0: (__quit, 'Quit experiment'),
+                 1: (__add_log_entry, 'Add entry to datalog'),
+                 2: (__remove_log_entry, 'Remove entry from datalog'),
+                 3: (__update_log_entry, 'Update datalog entry'),
+                 4: (__display_log, 'Display current datalog'),
+                 5: (__view_log_entry, 'View datalog entry'),
+                 6: (__save_log, 'Save datalog entries to files'),
+                 7: (__clear_log, 'Clear datalog')}
--- LightSource.py
+++ LightSource.py
@@ -14,7 +14,11 @@ Shannon entropy and frequency normalization.
 ## Imports 
 ######################################################################
 
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from termcolor import cprint
+# import piplates.DAQC2plate as DAQC2
 
 ######################################################################
 ## Authorship Information
@@ -34,12 +38,123 @@ __status__ = "Development"
 # Class Definition
 ######################################################################
 
-class Light_Source:
+class Light_Source(object):
     """Class docstrings go here TODO."""
-    def __init__(self, name):
+    def __init__(self, wavelength_nm, s_type="LED"):
         """Class method docstrings go here TODO."""
-        self.name = name
+        self.__wavelength = wavelength_nm
+        self.__source_type = s_type
+        self.__source_df = None
+        self.__stop_voltage = 0
+        return
+    
+    def __str__(self):
+        return ('Type: {0: >5},   λ: {1: >6.2f} nm,   V_s: {2: >5.3f} V'.format(self.__source_type, self.__wavelength, self.__stop_voltage))
 
-    def get_name(self):
-        """Class method docstrings go here TODO."""
-        return self.name
+    def get_type(self):
+        return self.__source_type
+
+    def get_wavelength(self):
+        return self.__wavelength
+
+    def get_data(self):
+        return self.__source_df
+
+    def get_stopping_volage(self):
+        return self.__stop_volatge
+
+    def get_color(self):
+        λ = self.__wavelength
+        if (λ >= 400 and λ < 450):
+            return 'darkviolet'
+        elif (λ >= 450 and λ < 500):
+            return 'blue'
+        elif (λ >= 500 and λ < 570):
+            return 'forestgreen'
+        elif (λ >= 570 and λ < 590):
+            return 'gold'
+        elif (λ >= 590 and λ < 610):
+            return 'darkorange'
+        elif (λ >= 610 and λ <= 700):
+            return 'red'
+        else:
+            return 'black'
+
+    def __get_max_voltage(self):
+        λ = self.__wavelength
+        if (λ >= 400 and λ < 450):
+            return 1.8
+        elif (λ >= 450 and λ < 500):
+            return 1.5
+        elif (λ >= 500 and λ < 570):
+            return 1.2
+        elif (λ >= 570 and λ < 590):
+            return 0.9
+        elif (λ >= 590 and λ < 610):
+            return 0.7
+        elif (λ >= 610 and λ <= 700):
+            return 0.6
+        else:
+            return 2
+
+    def __calc_stop_volage(self):
+        retarding_voltage = np.array(self.__source_df['V_r'], dtype='float').reshape(-1,1)
+        photocurrent = np.array(self.__source_df['I_φ'], dtype='float').reshape(-1,1)
+        index = -1
+        for i in range(photocurrent.size):
+            if (photocurrent[i] >= 0):
+                index = i
+                break
+        self.__stop_voltage = retarding_voltage[index][0]
+        return
+
+    def __create_source_df(source_data):
+        source_df = pd.DataFrame([], columns=['V_r', 'I_ub', 'I_b', 'I_φ'])
+        source_df['V_r'] = source_data[:,0]
+        source_df['I_ub'] = source_data[:,1]
+        source_df['I_b'] = source_data[:,2]
+        source_df['I_φ'] = source_data[:,1] + source_data[:,2]
+        self.__source_df = source_df.sort_values(by=['V_r'])
+        return
+
+    def collect_data(self):
+        voltages = np.linspace(0, self.__get_max_voltage(), num=1500)
+        np.random.shuffle(voltages)
+        source_data = np.zeros((voltages.size,3))
+
+        dark_current = 0
+        while (True):
+            try:
+                dark_current = float(input('Enter the dark current value in V: '))
+                break
+            except:
+                cprint('ERROR: invalid input: dark current must be a real number', 'red')
+
+        input("Press ENTER to begin data collection...")
+        for i in range(voltages.size):
+            V_r = voltages[i]
+            source_data[i,0] = V_r
+            # DAQC2.setDAC(0,0,V_r)
+            # V_p = DAQC2.getADC(0,0) - DAQC2.getADC(0,1)
+            source_data[i,1] = V_p
+            source_data[i,2] = dark_current
+        self.__create_source_df(source_data)
+        self.__calc_stop_volage()
+        return
+
+    def load_data_from_csv(self, filename):
+        self.__source_df = pd.read_csv(filename)
+        self.__calc_stop_volage()
+        return
+
+    def plot_data(self):
+        retarding_voltage = np.array(self.__source_df['V_r'], dtype='float').reshape(-1,1)
+        photocurrent = np.array(self.__source_df['I_φ'], dtype='float').reshape(-1,1)
+        plt.scatter(retarding_voltage, photocurrent, color='black')
+        plt.title('{0: .2f}nm {1:} Stopping Voltage'.format(self.__wavelength, self.__source_type), fontsize=18)
+        plt.xlabel('Retarding Voltage (V)', fontsize=14)
+        plt.ylabel('Photocurrent (µA)', fontsize=14)
+        plt.gca().invert_yaxis()
+        plt.grid()
+        plt.show()
+        return
